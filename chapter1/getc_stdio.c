c9a1d6d454408de6c14761a928cb0f6efc942268
XWHQSJ <xwhqsj@gmail.com>
2019-05-23 00:19:25 +0800

    add chapter1 stdin & stdout

---
+++ chapter1/getc_stdio.c
@@ -0,0 +1,24 @@
+#include "apue.h"
+#include "error.h"
+
+int
+main(void)
+{
+    int c;
+    
+    while((c = getc(stdin)) != EOF)
+    {
+        if(putc(c, stdout) == EOF)
+        {
+            err_sys("output error");
+        }
+    }
+    
+    if(ferror(stdin))
+    {
+        err_sys("input error");
+    }
+    
+    exit(0);
+}
+
