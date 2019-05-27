ee7bed1c516c71c028fb30858d5be9a949a67023
XWHQSJ <xwhqsj@gmail.com>
2019-05-27 12:49:55 +0800

    add error_msg.c

---
+++ chapter1/error_msg.c
@@ -0,0 +1,12 @@
+#include "apue.h"
+#include "error.h"
+#include <errno.h>
+
+int
+main(int argc, char *argv[])
+{
+    fprintf(stderr, "EACCES: %s\n", strerror(EACCES));
+    errno = ENOENT;
+    perror(argv[0]);
+    exit(0);
+}
