53503e1715cdb2eec1a58e009b430e6434db3b0c
XWHQSJ <xwhqsj@gmail.com>
2019-06-03 22:21:15 +0800

    add offset_stdin.c

---
+++ chapter3/offset_stdin.c
@@ -0,0 +1,16 @@
+#include "apue.h"
+#include "error.h"
+
+int main(void)
+{
+    if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
+    {
+        printf("cannot seek\n");
+    }
+    else
+    {
+        printf("seek OK\n");
+    }
+    
+    exit(0);
+}
