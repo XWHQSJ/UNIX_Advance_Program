e6195cbfcda890f8894828526d3ca0377f2f1f95
XWHQSJ <xwhqsj@gmail.com>
2019-06-12 18:47:54 +0800

    add unlink_file.c

---
+++ chapter4/unlink_file.c
@@ -0,0 +1,23 @@
+#include "apue.h"
+#include "error.h"
+#include <fcntl.h>
+
+int main(void)
+{
+    if (open("tempfile", O_RDWR) < 0)
+    {
+        err_sys("open error\n");
+    }
+
+    if (unlink("tempfile") < 0)
+    {
+        err_sys("unlink error");
+    }
+
+    printf("file unlinked\n");
+
+    sleep(15);
+    printf("done\n");
+
+    exit(0);
+}
