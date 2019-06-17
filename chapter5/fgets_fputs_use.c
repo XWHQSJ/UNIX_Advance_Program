ba916adf6195a097a204278394bd11af1c894adb
XWHQSJ <xwhqsj@gmail.com>
2019-06-17 13:33:21 +0800

    add fgets_fputs_use.c

--- chapter5/fgets_fputs_use.c
+++ chapter5/fgets_fputs_use.c
@@ -9,13 +9,13 @@ int main(void)
     {
         if (fputs(buf, stdout) == EOF)
         {
-            err_sys("output error");
+            err_sys("output error!");
         }
     }
 
     if (ferror(stdin))
     {
-        err_sys("input error");
+        err_sys("input error!");
     }
 
     exit(0);
