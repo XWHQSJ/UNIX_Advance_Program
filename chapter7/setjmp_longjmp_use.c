c886a396f32567afb86d205f91a33d683e593876
XWHQSJ <xwhqsj@gmail.com>
2019-06-24 23:51:08 +0800

    add setjmp_longjmp_use.c

---
+++ chapter7/setjmp_longjmp_use.c
@@ -0,0 +1,70 @@
+#include "apue.h"
+#include <setjmp.h>
+
+#define TOK_ADD 5
+
+jmp_buf jmpbuffer;
+
+void do_line(char *);
+void cmd_add(void);
+int get_token(void);
+
+int main(void)
+{
+    char line[MAXLINE];
+
+    if (setjmp(jmpbuffer) != 0)
+    {
+        printf("error");
+    }
+
+    while(fgets(line, MAXLINE, stdin) != NULL)
+    {
+        do_line(line);
+    }
+
+    exit(0);
+}
+
+// global pointer for get_token()
+char *tok_ptr;
+
+// process one line of input
+void do_line(char *ptr)
+{
+    int cmd;
+
+    tok_ptr = ptr;
+    while((cmd = get_token()) > 0)
+    {
+        // one case for each command
+        switch (cmd)
+        {
+        case TOK_ADD:
+            cmd_add();
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void cmd_add(void)
+{
+    int token;
+
+    token = get_token();
+
+    // an error has occurred
+    if (token < 0)
+    {
+        longjmp(jmpbuffer, 1);
+    }
+
+    /* rest of processing for this command */
+}
+
+int get_token(void)
+{
+    /* fetch next token from line pointed to by tok_ptr */
+}
