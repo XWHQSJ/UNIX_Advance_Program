38b1f0648d0d1c6966bac49548c43627f5e66b5b
XWHQSJ <xwhqsj@gmail.com>
2019-06-26 18:45:14 +0800

    add exit_status.c

---
+++ chapter8/exit_status.c
@@ -0,0 +1,84 @@
+#include "apue.h"
+#include "error.h"
+#include <sys/wait.h>
+
+void pr_exit(int status)
+{
+    if (WIFEXITED(status))
+    {
+        printf("normal termination, exit status = %d\n", WEXITSTATUS(status));
+    }
+    else if (WIFSIGNALED(status))
+    {
+        printf("abnormal termination, signal number = %d%s\n", WTERMSIG(status),
+#ifdef WCOREDUMP
+    __WCOREDUMP(status) ? " (core file generated)" : "");
+#else
+    "");
+#endif
+    }
+    else if (WIFSTOPPED(status))
+    {
+        printf("child stopped, signal number = %d\n", WSTOPSIG(status));
+    }
+}
+
+int main(void)
+{
+    pid_t pid;
+    int status;
+
+    if ((pid = fork()) < 0)
+    {
+        err_sys("fork error");
+    }
+    else if (pid == 0)
+    {
+        // child
+        exit(7);
+    }
+
+    // wait for child
+    if (wait(&status) != pid)
+    {
+        err_sys("wait error");
+    }
+    // print its status
+    pr_exit(status);
+
+    if ((pid = fork()) < 0)
+    {
+        err_sys("fork error");
+    }
+    else if (pid == 0)
+    {
+        // child
+        // generates SIGABRT
+        abort();
+    }
+
+    // wait for child
+    if (wait(&status) != pid)
+    {
+        err_sys("wait error");
+    }
+    pr_exit(status);
+
+    if ((pid = fork()) < 0)
+    {
+        err_sys("fork error");
+    }
+    else if (pid == 0)
+    {
+        // divide by 0 generates SIGFPE
+        status /= 0;
+    }
+
+    if (wait(&status) != pid)
+    {
+        err_sys("wait error");
+    }
+    pr_exit(status);
+
+    exit(0);
+}
