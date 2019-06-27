ed2c4c1535f44168a7799cc10a03b47e216976aa
XWHQSJ <xwhqsj@gmail.com>
2019-06-27 15:53:42 +0800

    add echo_all.c

--- chapter8/exec_use.c
+++ chapter8/exec_use.c
@@ -14,7 +14,7 @@ int main(void)
     }
     else if(pid == 0)
     {
-        if (execle("/home/wanhui/bin/echoall", "echoall", "myarg1", "MY ARG2", (char *)0, env_init) < 0)
+        if (execle("/home/wanhui/UNIX_Advance_Program/chapter8/echo_all", "echo_all", "myarg1", "MY ARG2", (char *)0, env_init) < 0)
         {
             err_sys("execle error\n");
         }
@@ -31,7 +31,7 @@ int main(void)
     }
     else if (pid == 0)
     {
-        if (execlp("echoall", "echoall", "only 1 arg", (char *)0) < 0)
+        if (execlp("/home/wanhui/UNIX_Advance_Program/chapter8/echo_all", "echo_all", "only 1 arg", (char *)0) < 0)
         {
             err_sys("execlp error\n");
         }
