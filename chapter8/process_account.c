a115e9b295b1814239263bde55d17d72494f9776
XWHQSJ <xwhqsj@gmail.com>
2019-06-28 13:22:51 +0800

    add process_account.c

---
+++ chapter8/process_account.c
@@ -0,0 +1,51 @@
+#include "apue.h"
+#include "error.h"
+
+int main(void)
+{
+    pid_t pid;
+
+    if((pid = fork()) < 0)
+    {
+        err_sys("fork error\n");
+    }
+    else if (pid != 0)
+    {
+        sleep(2);
+        exit(2);
+    }
+
+    if ((pid = fork()) < 0)
+    {
+        err_sys("fork error\n");
+    }
+    else if (pid != 0)
+    {
+        sleep(4);
+        abort();
+    }
+
+    if ((pid = fork()) < 0)
+    {
+        err_sys("fork error\n");
+    }
+    else if (pid != 0)
+    {
+        execl("/bin/dd", "dd", "if=/etc/passwd", "of=/dev/null", NULL);
+        exit(7);
+    }
+
+    if((pid = fork()) < 0)
+    {
+        err_sys("fork error\n");
+    }
+    else if (pid != 0)
+    {
+        sleep(8);
+        exit(0);
+    }
+
+    sleep(6);
+    kill(getpid(), SIGKILL);
+    exit(6);
+}
