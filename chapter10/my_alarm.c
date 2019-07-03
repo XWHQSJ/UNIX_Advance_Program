375aa3d2c82dc2c9530258a22bce516accd3e69d
XWHQSJ <xwhqsj@gmail.com>
2019-07-03 22:43:41 +0800

    add my_alarm.c

---
+++ chapter10/my_alarm.c
@@ -0,0 +1,38 @@
+#include "apue.h"
+#include "error.h"
+#include <pwd.h>
+
+static void my_alarm(int signo)
+{
+    struct passwd *rootptr;
+
+    printf("in signal handler\n");
+    
+    if ((rootptr = getpwnam("root")) == NULL)
+    {
+        err_sys("getpwnam(root) error\n");
+    }
+    
+    alarm(1);
+}
+
+int main(void)
+{
+    struct passwd *ptr;
+
+    signal(SIGALRM, my_alarm);
+    alarm(1);
+
+    for(;;)
+    {
+        if ((ptr = getpwnam("wanhui")) == NULL)
+        {
+            err_sys("getpwnam error");
+        }
+
+        if (strcmp(ptr->pw_name, "wanhui") != 0)
+        {
+            printf("return value corrupted!, pw_name = %s\n", ptr->pw_name);
+        }
+    }
+}
