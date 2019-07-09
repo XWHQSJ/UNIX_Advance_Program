e62d79c8a61e8ad68275264bed4d0e39b7652a61
XWHQSJ <xwhqsj@gmail.com>
2019-07-09 18:10:31 +0800

    add handle_SIGTSTP.c

---
+++ chapter10/handle_SIGTSTP.c
@@ -0,0 +1,54 @@
+#include "apue.h"
+#include "error.h"
+
+#define BUFFSIZE 1024
+
+static void sig_tstp(int signo)
+{
+    sigset_t mask;
+
+    /* move cursor to lower left corner, reset tty mode... */
+    /*
+    * Unblock SIGTSTP, since it's blocked while we're handling it.
+     */
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGTSTP);
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+
+    signal(SIGTSTP, SIG_DFL);   // reset disposition to default
+    kill(getpid(), SIGTSTP);    // and send the signal to ourself
+
+    /* we won't return from the kill until we're continued */
+    signal(SIGTSTP, sig_tstp);  // reestablish signal handler
+
+    /* reset tty mode, redraw screen... */
+}
+
+int main(void)
+{
+    int n;
+    char buf[BUFFSIZE];
+
+    /*
+    * Only catch SIGTSTP if we're renning with a job-control shell.
+     */
+    if(signal(SIGTSTP, SIG_IGN) == SIG_DFL)
+    {
+        signal(SIGTSTP, sig_tstp);
+    }
+
+    while((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+    {
+        if(write(STDOUT_FILENO, buf, n) != n)
+        {
+            err_sys("write error\n");
+        }
+    }
+
+    if(n < 0)
+    {
+        err_sys("read error\n");
+    }
+
+    exit(0);
+}
