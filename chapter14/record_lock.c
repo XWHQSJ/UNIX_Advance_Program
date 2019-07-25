a7718e4511f955577a94ccc557af209e88ede957
XWHQSJ <xwhqsj@gmail.com>
2019-07-25 10:44:15 +0800

    additional record_lock.c

--- chapter14/record_lock.c
+++ chapter14/record_lock.c
@@ -10,6 +10,27 @@ void sigint(int signo)
 
 }
 
+Sigfunc * signal_intr(int signo, Sigfunc *func)
+{
+    struct sigaction act, oact;
+
+    act.sa_handler = func;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+
+#ifdef SA_INTERRUPT
+    act.flags |= SA_INTERRUPT;
+#endif
+
+    if(sigaction(signo, &act, &oact) < 0)
+    {
+        return (SIG_ERR);
+    }
+
+    return (oact.sa_handler);
+
+}
+
 int main(void)
 {
     pid_t pid1, pid2, pid3;
