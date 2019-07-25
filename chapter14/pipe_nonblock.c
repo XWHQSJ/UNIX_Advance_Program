9c97aedc221c7a12c413060fb0f5c4db52432b00
XWHQSJ <xwhqsj@gmail.com>
2019-07-25 11:20:55 +0800

    additional pipe_nonblock.c

---
+++ chapter14/pipe_nonblock.c
@@ -0,0 +1,29 @@
+#include "apue.h"
+#include "error.h"
+#include "set_fl.h"
+#include <fcntl.h>
+
+int main(void)
+{
+    int i, n;
+    int fd[2];
+
+    if(pipe(fd) < 0)
+    {
+        err_sys("pipe error");
+    }
+    set_fl(fd[1], O_NONBLOCK);
+
+    /* write 1 byte at a time until pipe is full */
+    for(n = 0; ; n++)
+    {
+        if((i = write(fd[1], "a", 1)) != 1)
+        {
+            printf("write ret %d, ", i);
+            break;
+        }
+    }
+
+    printf("pipe capacity = %d\n", n);
+    exit(0);
+}
