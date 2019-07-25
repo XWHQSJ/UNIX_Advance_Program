32f67e4772fa4d1c4e86396f614e6f16144dc0db
XWHQSJ <xwhqsj@gmail.com>
2019-07-25 11:05:32 +0800

    additional poll_sleep_us.h

---
+++ chapter14/poll_sleep_us.h
@@ -0,0 +1,14 @@
+#include "apue.h"
+#include <poll.h>
+
+void sleep_us(unsigned int nusecs)
+{
+    struct pollfd dummy;
+    int timeout;
+
+    if((timeout = nusecs / 1000) <= 0)
+    {
+        timeout = 1;
+    }
+    poll(&dummy, 0, timeout);
+}
