2c649c142603c60567f73918b231c1bf4149d29b
XWHQSJ <xwhqsj@gmail.com>
2019-07-29 11:23:59 +0800

    correct add2_std.c

--- chapter15/add2_std.c
+++ chapter15/add2_std.c
@@ -6,6 +6,16 @@ int main(void)
     int int1, int2;
     char line[MAXLINE];
 
+    if(setvbuf(stdin, NULL, _IOLBF, 0) != 0)
+    {
+        err_sys("setvbuf error");
+    }
+
+    if(setvbuf(stdout, NULL, _IOLBF, 0) != 0)
+    {
+        err_sys("setvbuf error");
+    }
+
     while(fgets(line, MAXLINE, stdin) != NULL)
     {
         if(sscanf(line, "%d%d", &int1, &int2) == 2)
