45e5fa1582087defe3ebf8cb286a7526dcb0178b
XWHQSJ <xwhqsj@gmail.com>
2019-07-31 14:00:08 +0800

    add nonblock_fifo.c

---
+++ chapter15/clr_fl.h
@@ -0,0 +1,19 @@
+#include "error.h"
+#include <fcntl.h>
+
+void clr_fl(int fd, int flags)
+{
+    int val;
+
+    if((val = fcntl(fd, F_GETFL, 0)) < 0)
+    {
+        err_sys("fcntl F_GETFL error");
+    }
+
+    val &= ~flags;
+
+    if(fcntl(fd, F_SETFL, val) < 0)
+    {
+        err_sys("fcntl F_SETFL error");
+    }
+}
