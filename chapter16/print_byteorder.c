b5728c249f84245a25c7fd04043d5146cfe01bc9
XWHQSJ <xwhqsj@gmail.com>
2019-08-04 22:13:48 +0800

    add practices.md & print_byteorder.c & a.out

---
+++ chapter16/print_byteorder.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+int main(void)
+{
+    uint32_t i = 0x04030201;
+    unsigned char *cp = (unsigned char *)&i;
+
+    if(*cp == 1)
+    {
+        printf("little-endian\n");
+    }
+    else if(*cp = 4)
+    {
+        printf("bit-endian\n");
+    }
+    else
+    {
+        printf("who knows?\n");
+    }
+    exit(0);
+}
