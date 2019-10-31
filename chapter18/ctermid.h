a2d4e8bbdd910e1e4c3cd69ec320912efd2ada9d
xwhqsj <xwhqsj@gmail.com>
2019-10-31 13:38:57 +0800

    add ctermid.h

---
+++ chapter18/ctermid.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+static char ctermid_name[L_ctermid];
+
+char* ctermid(char *str)
+{
+    if(str == nullptr)
+    {
+        str = ctermid_name;
+    }
+
+    return (strcpy(str, "/dev/tty"));
+}
